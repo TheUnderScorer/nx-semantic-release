05492c72fb2bfcea007866770dbdb81954954b8c
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-05-15 14:25:05 +0200

    refactor: Use nxrelease.config.js file for config in test repo

---
+++ test-repos/app/nxrelease.config.dist.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dryRun: false,
+  repositoryUrl:
+    'file:///Users/przemyslawzydek/WebstormProjects/nx-semantic-release/git-server/repos/project.git',
+  github: false,
+  ci: false,
+  branches: ['*'],
+};
