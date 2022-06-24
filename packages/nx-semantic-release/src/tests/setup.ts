736bb89e4fe3ccf272747ca535d142103b2df579
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-06-24 17:03:14 +0200

    test: remove CI env

--- packages/nx-semantic-release/src/tests/setup.ts
+++ packages/nx-semantic-release/src/tests/setup.ts
@@ -1,8 +1,20 @@
 import path from 'path';
 
+const ciEnvToRemove = [
+  'GITHUB_EVENT_NAME',
+  'GITHUB_RUN_ID',
+  'GITHUB_REPOSITORY',
+  'GITHUB_WORKSPACE',
+  'GITHUB_ACTIONS',
+];
+
 Object.assign(process.env, {
   NX_WORKSPACE_ROOT_PATH: path.resolve(
     __dirname,
     '../../../../tmp/nx-e2e/proj'
   ),
+});
+
+ciEnvToRemove.forEach((key) => {
+  delete process.env[key];
 });
