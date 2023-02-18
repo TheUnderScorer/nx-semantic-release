faa298bb726c41d5979eae1b23a4682784d151f6
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-02-18 14:42:56 +0100

    test: use correct root

--- packages/nx-semantic-release/src/tests/setup.ts
+++ packages/nx-semantic-release/src/tests/setup.ts
@@ -1,29 +1,8 @@
 import path from 'path';
-import fs from 'fs';
 
 // Note: we cannot use tmpProjPath() here, because it uses the NX_WORKSPACE_ROOT_PATH, so it will cause infinite loop
-const possibleRoots = [
-  path.resolve(__dirname, '../../tmp/nx-e2e/proj'),
-  path.resolve(__dirname, '../../../tmp/nx-e2e/proj'),
-  path.resolve(__dirname, '../../../../tmp/nx-e2e/proj'),
-];
+const root = path.resolve(__dirname, '../../../../tmp/nx-e2e/proj');
 
-let hasRoot = false;
-
-for (const possibleRoot of possibleRoots) {
-  if (fs.existsSync(possibleRoot)) {
-    console.info(`Found test project root at ${possibleRoot}`);
-
-    Object.assign(process.env, {
-      NX_WORKSPACE_ROOT_PATH: possibleRoot,
-    });
-
-    hasRoot = true;
-  }
-}
-
-if (!hasRoot) {
-  console.error('Failed to find Nx workspace root');
-
-  process.exit(1);
-}
+Object.assign(process.env, {
+  NX_WORKSPACE_ROOT_PATH: root,
+});
