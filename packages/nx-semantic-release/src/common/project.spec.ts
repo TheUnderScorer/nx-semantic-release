49045b4fb1a94533098274fc168d45c0a22c8652
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-02-19 12:14:17 +0100

    build(dev-deps): bump typescript

--- packages/nx-semantic-release/src/common/project.spec.ts
+++ packages/nx-semantic-release/src/common/project.spec.ts
@@ -1,61 +1,69 @@
-import path from 'path';
-import { getProject, getProjectDependencies, getProjectRoot } from './project';
-import { readTestAppWorkspace } from '../tests/utils';
-import { tmpProjPath } from '@nrwl/nx-plugin/testing';
-import { setupTestRepo } from '../tests/setup-test-repo';
-import { cleanupTestRepo } from '../tests/cleanup-test-repo';
+import path from "path";
+import { getProject, GetProjectContext, getProjectDependencies, getProjectRoot } from "./project";
+import { cleanupTestRepo } from "../tests/cleanup-test-repo";
+import { setupTestRepo } from "../tests/setup-test-repo";
+import { tmpProjPath } from "@nrwl/nx-plugin/testing";
 
-beforeAll(async () => {
-  cleanupTestRepo();
+describe("project", () => {
+  beforeAll(async () => {
+    cleanupTestRepo();
 
-  await setupTestRepo();
-});
-
-afterAll(async () => {
-  cleanupTestRepo();
-});
-
-describe('getProjectDependencies', () => {
-  it.each<{ projectName: string; expectedDependencies: string[] }>([
-    {
-      projectName: 'app-a',
-      expectedDependencies: ['lib-a', 'lib-a-dependency', 'common-lib'],
-    },
-    {
-      projectName: 'app-b',
-      expectedDependencies: ['common-lib'],
-    },
-    {
-      projectName: 'common-lib',
-      expectedDependencies: [],
-    },
-  ])('should return correct dependencies', async (data) => {
-    const result = await getProjectDependencies(data.projectName);
-
-    expect(result.dependencies).toEqual(data.expectedDependencies);
+    await setupTestRepo();
   });
-});
 
-describe('getProjectRoot', () => {
-  it('should return correct project root', () => {
-    const workspace = readTestAppWorkspace();
-
-    const result = getProjectRoot(workspace.projects['app-a'], tmpProjPath());
-
-    expect(result).toEqual(path.join(tmpProjPath(), '/apps/app-a'));
+  afterAll(() => {
+    cleanupTestRepo();
   });
-});
 
-describe('getProject', () => {
-  it('should return project from workspace', () => {
-    const projectsConfigurations = readTestAppWorkspace();
+  const projectsConfigurations = {
+    version: 1,
+    projects: {
+      "app-a": {
+        root: "root"
+      }
+    }
+  } satisfies GetProjectContext["projectsConfigurations"];
 
-    expect(
-      getProject({
-        cwd: tmpProjPath(),
-        projectsConfigurations,
-        projectName: 'app-a',
-      })
-    ).toEqual(projectsConfigurations.projects['app-a']);
+  describe("getProjectDependencies", () => {
+    it.each<{ projectName: string; expectedDependencies: string[] }>([
+      {
+        projectName: "app-a",
+        expectedDependencies: ["lib-a", "lib-a-dependency", "common-lib"]
+      },
+      {
+        projectName: "app-b",
+        expectedDependencies: ["common-lib"]
+      },
+      {
+        projectName: "common-lib",
+        expectedDependencies: []
+      }
+    ])("should return correct dependencies", async (data) => {
+      const result = await getProjectDependencies(data.projectName);
+
+      expect(result.dependencies).toEqual(data.expectedDependencies);
+    });
+  });
+
+  describe("getProjectRoot", () => {
+    it("should return correct project root", () => {
+      const cwd = tmpProjPath();
+
+      const result = getProjectRoot(projectsConfigurations.projects["app-a"], cwd);
+
+      expect(result).toEqual(path.join(cwd, projectsConfigurations.projects["app-a"].root));
+    });
+  });
+
+  describe("getProject", () => {
+    it("should return project from workspace", () => {
+      expect(
+        getProject({
+          cwd: tmpProjPath(),
+          projectsConfigurations,
+          projectName: "app-a"
+        })
+      ).toEqual(projectsConfigurations.projects["app-a"]);
+    });
   });
 });
