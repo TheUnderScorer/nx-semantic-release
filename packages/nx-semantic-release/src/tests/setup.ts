820136711ccd0c06aeb5dfe084ac8255e0d8fb43
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-02-19 12:44:14 +0100

    refactor: simplify setting nx workspace root

--- packages/nx-semantic-release/src/tests/setup.ts
+++ packages/nx-semantic-release/src/tests/setup.ts
@@ -1,8 +1,5 @@
-import path from 'path';
-
-// Note: we cannot use tmpProjPath() here, because it uses the NX_WORKSPACE_ROOT_PATH, so it will cause infinite loop
-const root = path.resolve(__dirname, '../../../../tmp/nx-e2e/proj');
+import { tmpProjPath } from '@nrwl/nx-plugin/testing';
 
 Object.assign(process.env, {
-  NX_WORKSPACE_ROOT_PATH: root,
+  NX_WORKSPACE_ROOT_PATH: tmpProjPath(),
 });
