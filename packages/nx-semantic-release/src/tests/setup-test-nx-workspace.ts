134f08bd4d6d1a3d9a3f1415c565de4f70ef84d3
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-02-19 19:20:49 +0100

    build: fix invalid dist path

--- packages/nx-semantic-release/src/tests/setup-test-nx-workspace.ts
+++ packages/nx-semantic-release/src/tests/setup-test-nx-workspace.ts
@@ -1,41 +1,17 @@
-import {
-  ensureNxProject,
-  tmpProjPath,
-  updateFile,
-} from '@nrwl/nx-plugin/testing';
-import { PackageJson } from 'type-fest';
-import path from 'path';
-import { runCommandsInTestProj } from './setup-test-repo';
+import { ensureNxProject, tmpProjPath } from '@nrwl/nx-plugin/testing';
 import fs from 'fs-extra';
 
 export async function setupTestNxWorkspace() {
   try {
     console.info('Setting up at test nx workspace at:', tmpProjPath());
 
-    const distPath = path.resolve(
-      __dirname,
-      '../../../..',
-      'dist/packages/nx-semantic-release'
-    );
+    const distPath = 'dist/packages/nx-semantic-release';
 
     if (!fs.existsSync(distPath)) {
       throw new Error(`Nx plugin dist folder does not exist at: ${distPath}`);
     }
 
     ensureNxProject('@theunderscorer/nx-semantic-release', distPath);
-
-    updateFile('package.json', (contents) => {
-      const pkg = JSON.parse(contents) as PackageJson;
-
-      pkg.devDependencies = {
-        ...pkg.devDependencies,
-        '@theunderscorer/nx-semantic-release': `file:${distPath}`,
-      };
-
-      return JSON.stringify(pkg, null, 2);
-    });
-
-    await runCommandsInTestProj(['npm install']);
   } catch (error) {
     console.error('Failed to setup test Nx workspace', error);
 
--- packages/nx-semantic-release/src/tests/setup.ts
+++ packages/nx-semantic-release/src/tests/setup.ts
@@ -1,4 +1,6 @@
+/*
 Object.assign(process.env, {
   // Note: We can't use tmpProjPath() here, because it can cause infinite loop
   NX_WORKSPACE_ROOT_PATH: `${process.cwd()}/tmp/nx-e2e/proj`,
 });
+*/
