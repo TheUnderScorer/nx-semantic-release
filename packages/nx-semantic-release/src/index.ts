12ad5680e49223c7f6d77b8a4608886b0f926e09
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-08-24 19:00:13 +0200

    fix: include missing es6-weak-map dependency

--- packages/nx-semantic-release/src/index.ts
+++ packages/nx-semantic-release/src/index.ts
@@ -1,1 +1,6 @@
+// TODO Resolve this dependency somehow
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import 'es6-weak-map';
+
 export * from './semantic-release-plugin';
